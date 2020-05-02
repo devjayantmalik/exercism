94bd58a9d63c8c67d77ba6e546e70b0354a57045
Jayant Malik <dev.jayantmalik@gmail.com>
2020-05-02 17:52:47 +0530

    finished isogram challenge

--- cpp/isogram/isogram.cpp
+++ cpp/isogram/isogram.cpp
@@ -2,4 +2,23 @@
 
 namespace isogram {
 
+	bool is_isogram(std::string word){
+		int len = sizeof(word);
+
+		if(word == "") return true;
+
+		for(int i=0; i< len; i++){
+			// Get the Current Character
+			char character = word[i];
+
+			// Check if current character exists in the word.
+			for(int j = 0; j < len; j++){
+				if(word[j] == character) return false;
+			}
+		}
+
+		return true;
+	}
+
+
 }  // namespace isogram
--- cpp/isogram/isogram.h
+++ cpp/isogram/isogram.h
@@ -1,7 +1,11 @@
 #if !defined(ISOGRAM_H)
 #define ISOGRAM_H
 
+#include <string>
+
 namespace isogram {
+
+bool is_isogram(std::string);
 
 }  // namespace isogram
 
